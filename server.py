33cba1cfa143a90d20e80119b33218798c6f633e
mythsunwind <sven@wire.com>
2021-01-14 22:43:28 +0100

    Fix syntax error

--- server.py
+++ server.py
@@ -57,7 +57,7 @@ def readlineFromClient(matrix, client, address):
                     else:
                         rgb = ImageColor.getcolor("#" + color, "RGB")
                     pixels[int(arguments[1]), int(arguments[2])] = rgb
-            else if len(arguments) == 1:
+            elif len(arguments) == 1:
                 if arguments[0] == "SIZE":
                     client.send("SIZE {} {}\n".format(size[0], size[1]).encode())
                     break
